cd8c0f7b808b565c0261c1605c46ce25bbdacfe3
yuncheol <yuncheol92@naver.com>
2021-08-21 17:48:44 +0900

    동일한 코드 스타일로 변경

--- src/main/java/kr/co/_29cm/homework/helper/EnvironmentHelper.java
+++ src/main/java/kr/co/_29cm/homework/helper/EnvironmentHelper.java
@@ -1,13 +1,15 @@
 package kr.co._29cm.homework.helper;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class EnvironmentHelper {
-    @Autowired
-    private Environment environment;
+
+    private final Environment environment;
 
     public Boolean isTestProfiles() {
         return "test".equals(environment.getActiveProfiles()[0]);
